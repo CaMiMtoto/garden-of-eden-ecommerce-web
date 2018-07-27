05f140362c16ba397e3dc58eea5a7009a199b967
paul <jeanpaulbyiringiro@gmail.com>
2018-07-27 16:07:48 +0200

    fix share

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,17 +102,22 @@
 
                         <ul class="newsletter-follow">
                             <li>
-                                <a href="https://www.facebook.com/MtotoCaMi" target="_blank"><i
+                                <a href="https://www.facebook.com/EdenofGardenProducer" target="_blank"><i
                                             class="fa fa-facebook"></i></a>
                             </li>
                             <li>
-                                <a href="https://twitter.com/JeanByiringiro" target="_blank"><i
+                                <a href="https://twitter.com/" target="_blank"><i
                                             class="fa fa-twitter"></i></a>
                             </li>
                             <li>
-                                <a href="https://www.instagram.com/jean_paul_camy/" target="_blank"><i
+                                <a href="https://www.instagram.com/garden_of_eden_produce/" target="_blank"><i
                                             class="fa fa-instagram"></i></a>
                             </li>
+                            <li>
+                                <a href="https://www.youtube.com/" target="_blank"><i
+                                            class="fa fa-youtube-play"></i></a>
+                            </li>
+
                         </ul>
                     </div>
                 </div>
